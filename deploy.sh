e1b3485a130debddbb8d9d558c15cb2a7a7c03a0
Kun Ei Kang <vtssogari@gmail.com>
2022-01-30 14:38:18 -0500

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,9 @@
 export K8S_NAMESPACE=default
+export UNLEASH_API_TOKEN=
+export UNLEASH_URL=
+export UNLEASH_PROJECT_NAME=
+export UNLEASH_PROXY_CLIENT_KEYS=
+export UNLEASH_ENVIRONMENT=development
+
 envsubst < deploy-unleash-proxy.yaml > deploy.yaml
 kubectl apply -f deploy.yaml
